5a11393b1561ac2ba4822d9a5656bac992133cec
aa407766499 <44485411+aa407766499@users.noreply.github.com>
2020-07-30 07:53:51 +0800

    Initial commit

---
+++ src/main/java/com/huzi/study/nacosspring/NacosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.huzi.study.nacosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huzi/study/nacosspring/NacosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huzi.study.nacosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
